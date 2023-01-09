cf69ac0fb467b99e69cfa11196ea8856145deea7
Anil Acar <101933654+Acarjs@users.noreply.github.com>
2023-01-09 08:49:09 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,16 @@
+//Filter - Create a new array by keeping the items that return true.
+
+let numbers = [3, 56, 2, 48, 5];
+
+//Newest version
+const newNumbers = numbers.filter(number => number < 10);
+console.log(newNumbers); //[2,3,5]
+
+//forEach
+var newNumbersForEach = [];
+numbers.forEach(function (num) {
+  if (num < 10) {
+    newNumbers.push(num);
+  }
+});
+console.log(newNumbersForEach); //[2,3,5]
